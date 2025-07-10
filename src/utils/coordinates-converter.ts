c3fa0b404ba5650c462cd09b2ce6bd2926b0f4fe
Kevin BIOJOUT <biojoutk1106@gmail.com>
2025-07-10 18:58:36 +0200

    fix coordinates 4

--- src/utils/coordinates-converter.ts
+++ src/utils/coordinates-converter.ts
@@ -1,4 +1,4 @@
 export const lambertToLatLong = (input: string) => {
 	const [lat, lon] = input.split(" ");
-	return { latitude: +lat / 100000, longitude: +lon / 100000 };
+	return { latitude: +lat / 100000000, longitude: +lon / 100000000 };
 };
