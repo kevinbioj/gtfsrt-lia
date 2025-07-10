adcc6fb241b32704b5242d6dca8278f0f0c5a3cb
Kevin BIOJOUT <biojoutk1106@gmail.com>
2025-07-10 18:36:33 +0200

    fix coordinates

--- src/utils/coordinates-converter.ts
+++ src/utils/coordinates-converter.ts
@@ -1,14 +1,15 @@
 import proj4 from "proj4";
 
 export const lambertToLatLong = (input: string) => {
-  const [x, y] = input.split(" ");
-  const coordinates = proj4(
-    "+proj=lcc +lat_1=46.8 +lat_0=46.8 +lon_0=0 +k_0=0.99987742 +x_0=600000 +y_0=2200000 +a=6378249.2 +b=6356515 +towgs84=-168,-60,320,0,0,0,0 +pm=paris +units=m +no_defs",
-    "+proj=longlat +datum=WGS84 +no_defs",
-    [+x, +y]
-  );
-  return {
-    latitude: coordinates[1],
-    longitude: coordinates[0],
-  };
+	const [x, y] = input.split(" ");
+	// as of 2025/07, coordinates are in meters so we / 100 to get the right result
+	const coordinates = proj4(
+		"+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs",
+		"+proj=longlat +datum=WGS84 +no_defs",
+		[+x / 100, +y / 100],
+	);
+	return {
+		latitude: coordinates[1],
+		longitude: coordinates[0],
+	};
 };
