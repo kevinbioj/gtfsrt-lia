f4f8b67db5dc047cbb237f2ab9baecc0d2602f59
Kevin BIOJOUT <biojoutk1106@gmail.com>
2025-07-10 18:54:33 +0200

    fix coordinates 2

--- src/utils/coordinates-converter.ts
+++ src/utils/coordinates-converter.ts
@@ -1,13 +1,16 @@
 import proj4 from "proj4";
 
+proj4.defs(
+	"EPSG:27572",
+	"+proj=lcc +lat_1=45.89891888888889 +lat_2=47.69601444444444 " +
+		"+lat_0=46.8 +lon_0=2.337229166666667 +x_0=600000 +y_0=2200000 " +
+		"+ellps=clrk80ign +units=m +no_defs",
+);
+
 export const lambertToLatLong = (input: string) => {
 	const [x, y] = input.split(" ");
 	// as of 2025/07, coordinates are in meters so we / 100 to get the right result
-	const coordinates = proj4(
-		"+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs",
-		"+proj=longlat +datum=WGS84 +no_defs",
-		[+x / 100, +y / 100],
-	);
+	const coordinates = proj4("EPSG:27572", "EPSG:4326", [+x / 1000, +y / 1000]);
 	return {
 		latitude: coordinates[1],
 		longitude: coordinates[0],
