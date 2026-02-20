36f1daf83dd401c04bf8f39aca575aa00b471c0b
Kevin BIOJOUT <biojoutk1106@gmail.com>
2026-02-20 07:14:59 +0100

    fix caching issue when computing operating trips

--- src/utils/is-service-operating-on.ts
+++ src/utils/is-service-operating-on.ts
@@ -2,21 +2,23 @@ import { Temporal } from "temporal-polyfill";
 
 import type { Service } from "../gtfs/import-resource.js";
 
-const cache = new Map<string, boolean>();
+const cache = new Map<string, { date: Temporal.PlainDate; operating: boolean }>();
 
 export function isServiceOperatingOn(service: Service, date: Temporal.PlainDate) {
 	const cached = cache.get(service.id);
-	if (cached !== undefined) {
+	if (cached?.date.equals(date)) {
 		return cached;
 	}
 
+	cache.delete(service.id);
+
 	if (service.includedDays.some((d) => d.equals(date))) {
-		cache.set(service.id, true);
+		cache.set(service.id, { date, operating: true });
 		return true;
 	}
 
 	if (service.excludedDays.some((d) => d.equals(date))) {
-		cache.set(service.id, false);
+		cache.set(service.id, { date, operating: false });
 		return false;
 	}
 
@@ -24,11 +26,11 @@ export function isServiceOperatingOn(service: Service, date: Temporal.PlainDate)
 		Temporal.PlainDate.compare(date, service.startDate) < 0 ||
 		Temporal.PlainDate.compare(date, service.endDate) > 0
 	) {
-		cache.set(service.id, false);
+		cache.set(service.id, { date, operating: false });
 		return false;
 	}
 
 	const workingOnDay = service.days[date.dayOfWeek];
-	cache.set(service.id, workingOnDay);
+	cache.set(service.id, { date, operating: workingOnDay });
 	return workingOnDay;
 }
